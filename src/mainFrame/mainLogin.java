d2f58cdefb0d1e2ac9de11bfd6ee5fdd57e09572
罗小其 <1537069101@qq.com>
2020-06-30 14:25:21 +0800

    QQ聊天室 java逐步开发

--- src/mainFrame/mainLogin.java
+++ src/mainFrame/mainLogin.java
@@ -14,12 +14,15 @@ public class mainLogin {
         up_Login up_login = new up_Login();
         up_login.setBounds(0,0,Constant.W,200);
 
+        dawn_Login dawn_login = new dawn_Login();
+        dawn_login.setBounds(0,200,Constant.W,400);
 
         main_login.add(up_login);
-
-        dawn_Login dawn_login = new dawn_Login();
-        dawn_login.setBounds(0,300,Constant.W,300);
         main_login.add(dawn_login);
+
+        main_login.setUndecorated(true);
+        main_login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        main_login.setVisible(true);
 
     }
     static class main_Login extends JFrame{
@@ -28,9 +31,7 @@ public class mainLogin {
         public main_Login() {
            this.setSize(Constant.W,Constant.H);
            this.setLayout(null);
-           this.setUndecorated(true);
-           this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-           this.setVisible(true);
+
 
 
 
@@ -75,18 +76,55 @@ public class mainLogin {
     static class dawn_Login extends JPanel{
         private JLabel img ;
         private JPanel jPanel1;
-        private JPanel jPanel2 ;
         private JButton Login;
+        private String name = "张三";
 
         public dawn_Login() {
+            this.setSize(800,400);
             this.setLayout(null);
             ImageIcon qq = new ImageIcon("src/imgs/qq.jpg");
             img = new JLabel(qq);
-            img.setBounds(100,300,qq.getIconWidth(),qq.getIconHeight());
+            img.setBounds(100,100,qq.getIconWidth(),qq.getIconHeight());
 
-//            jPanel1 = new JPanel(null);
+            jPanel1 = new JPanel();
+            JLabel jLabel = new JLabel("请输入你的账号                       注册账号");
+            jLabel.setFont(new Font("楷体",1,20));
+            jLabel.setBackground(Color.BLUE);
+            jLabel.setBounds(200,100,500,60);
 
+            JTextField jTextField = new JTextField(20);
+            jTextField.setPreferredSize(new Dimension(100,20));
+            JLabel jLabel1 = new JLabel("找回密码");
+            jLabel.setFont(new Font("楷体",0,20));
+            jLabel.setBackground(Color.BLUE);
+
+            jPanel1.add(jTextField);
+            jPanel1.add(jLabel1);
+            jPanel1.setBounds(200,170,500,100);
+
+            Login = new JButton("  登录  ");
+            Login.setBounds(200,270,500,60);
+
+            this.add(Login);
+            this.add(jPanel1);
+            this.add(jLabel);
             this.add(img);
+
+            Login.addActionListener(e -> {
+                if (jTextField.getText().equals(name)){
+
+                 System.exit(0);
+                }else{
+                    JOptionPane.showMessageDialog(new JPanel(),"你输入的密码或者账号错误","请重新输入",JOptionPane.WARNING_MESSAGE);
+                }
+
+            });
         }
+
+//        @Override
+//        public void paint(Graphics g) {
+//            super.paint(g);
+//       this.setBackground(Color.BLACK);
+//        }
     }
 }
